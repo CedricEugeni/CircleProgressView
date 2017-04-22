8613ed64859f4eb0069411dc0dab9d74d44219bd
Cédric Eugeni <cedric.eugeni@gmail.com>
2017-04-22 19:07:49 +0200

    Bootstrap project

---
+++ CircleProgressView/CircleProgressView.h
@@ -0,0 +1,19 @@
+//
+//  CircleProgressView.h
+//  CircleProgressView
+//
+//  Created by Cédric Eugeni on 22/04/2017.
+//  Copyright © 2017 cedric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircleProgressView.
+FOUNDATION_EXPORT double CircleProgressViewVersionNumber;
+
+//! Project version string for CircleProgressView.
+FOUNDATION_EXPORT const unsigned char CircleProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircleProgressView/PublicHeader.h>
+
+
